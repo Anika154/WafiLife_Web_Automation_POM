c13fe3b65d83ed268e958aabdc2f490cdebcc0d6
Anika Anjum <anika15-11488@diu.edu.bd>
2024-10-19 21:48:08 +0600

    updated author and Book details

---
+++ HomePage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.By;
+
+public class HomePage extends BasePage {
+    public String homePageURL = "https://www.wafilife.com/";
+    public By name = By.xpath("//input[@id='billing_first_name']");
+    public By phoneNumber = By.xpath("//input[@id='billing_phone']");
+    public By email = By.xpath("//input[@id='billing_email']");
+    public By area = By.xpath("//select[@id='billing_area']");
+    public By address = By.xpath("//textarea[@id='billing_address_1']");
+    public By finalCompleteOrder = By.xpath("//button[@id='place_order']");
+
+
+}
---
+++ TestHomePage.java
@@ -0,0 +1,34 @@
+package testCases;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import utilities.DriverSetUp;
+
+public class TestHomePage extends DriverSetUp {
+
+    HomePage homePage = new HomePage();
+
+    @Test (description = "Test Home Page")
+    @Description("Test Results after ADD TO Cart")
+     public void testHomePageProcess() {
+         getDriver().get(homePage.homePageURL);
+        homePage.writeOnElement(homePage.name,"Orin");
+        homePage.writeOnElement(homePage.phoneNumber,"01711369253");
+        homePage.writeOnElement(homePage.email,"orin@gmail.com");
+        homePage.selectWithVisibleText(homePage.area,"শ্যামলী");
+        homePage.writeOnElement(homePage.address,"house #9-e, road #1, shaymoli, 1207");
+
+//        js.executeScript("window.scrollBy(0,950)", "");
+        homePage.clickOnElement(homePage.finalCompleteOrder);
+        homePage.addScreenShot("Completing the Order");
+        Assert.assertTrue(getDriver().findElement(By.xpath("//p[contains(text(),'আপনাকে ধন্যবাদ। অর্ডারটি সাবমিট হয়েছে।')]")).isDisplayed(),"\"আপনাকে ধন্যবাদ। অর্ডারটি সাবমিট হয়েছে।");
+     }
+
+
+
+}
