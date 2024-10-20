825a1f05ebc0d0870cfbf390de2e4c9d157dac67
Anika Anjum <anika15-11488@diu.edu.bd>
2024-10-20 20:14:44 +0600

    Update TestHomePage.java

--- src/test/java/testCases/TestHomePage.java
+++ src/test/java/testCases/TestHomePage.java
@@ -13,21 +13,12 @@ public class TestHomePage extends DriverSetUp {
 
     HomePage homePage = new HomePage();
 
-    @Test (description = "Test Home Page")
-    @Description("Test Results after ADD TO Cart")
-     public void testHomePageProcess() throws InterruptedException {
-         getDriver().get(homePage.homePageURL);
-        homePage.writeOnElement(homePage.name,"Orin");
-        homePage.writeOnElement(homePage.phoneNumber,"01711369253");
-        homePage.writeOnElement(homePage.email,"orin@gmail.com");
-        homePage.selectWithVisibleText(homePage.area,"শ্যামলী");
-        homePage.writeOnElement(homePage.address,"house #9-e, road #1, shaymoli, 1207");
+//    @Test (description = "Test Home Page")
+//    @Description("Test Results after ADD TO Cart")
+//     public void testHomePageProcess() {
+//
+//     }
 
-//        js.executeScript("window.scrollBy(0,950)", "");
-        homePage.clickOnElement(homePage.finalCompleteOrders);
-        homePage.addScreenShot("Completing the Order");
-        Assert.assertTrue(getDriver().findElement(By.xpath("//p[contains(text(),'আপনাকে ধন্যবাদ। অর্ডারটি সাবমিট হয়েছে।')]")).isDisplayed(),"\"আপনাকে ধন্যবাদ। অর্ডারটি সাবমিট হয়েছে।");
-     }
 
 
 
