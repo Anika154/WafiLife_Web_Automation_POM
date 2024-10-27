795fbf9d18f15c2319b6822fdc3c1df274639517
Anika Anjum <anika15-11488@diu.edu.bd>
2024-10-27 17:13:49 +0600

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.By;
 
 public class HomePage extends BasePage {
     public String homePageURL = "https://www.wafilife.com/";
+    public By searchInput = By.xpath("//div[@class='header_search']//input[@id='freeSearchBoxWeb']");
+    public By searchBtn = By.xpath("//div[@class='header_search']//button[@type='submit']");
+    public By selectBook = By.xpath("//a[contains(text(),'বৈষম্য বিরোধী আন্দোলন')]");
 
 
 
